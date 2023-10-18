5e0544b0b3f8250dca87ca55862e7eac39778277
MesakDuduCoder <duduaoa99@gmail.com>
2023-10-19 00:19:30 +0530

    Add helper

---
+++ app/helpers/categories_helper.rb
@@ -0,0 +1,21 @@
+module CategoriesHelper
+  def bootstrap_icon_options
+    [
+      ['Select an Icon', ''],
+      ['Books', 'bi bi-book'],
+      ['Food', 'bi bi-egg-fried'],
+      ['Rent', 'bi bi-house-fill'],
+      ['Car', 'bi bi-car-front-fill'],
+      ['Gas', 'bi bi-fuel-pump'],
+      ['Electricity', 'bi bi-lightbulb-fill'],
+      ['Water', 'bi bi-droplet-fill'],
+      ['Phone', 'bi bi-phone'],
+      ['Internet', 'bi bi-wifi'],
+      ['Clothes', 'bi bi-handbag-fill'],
+      ['Online Shopping', 'bi bi-cart-fill'],
+      ['Health', 'bi bi-heart-fill'],
+      ['Gym', 'bi bi-person-arms-up'],
+      ['Travel', 'bi bi-luggage-fill']
+    ]
+  end
+end
