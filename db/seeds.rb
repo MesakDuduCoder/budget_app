f8276194cfc895bcfbd7c3f450d74b5dcefd5e20
MesakDuduCoder <duduaoa99@gmail.com>
2023-10-18 12:44:19 +0530

    Remove seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,1 @@
-user = User.create!(name: "John Doe")
-category = user.categories.create!(name: "Food", icon: "icon")
-payment = user.payments.create!(name: "ForFood", amount: 10)
-category.payments << payment
+
