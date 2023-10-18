495925af87e53b19406eb6b5c33546a0137f31aa
MesakDuduCoder <duduaoa99@gmail.com>
2023-10-19 00:19:37 +0530

    Update helper

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  helper_method :bootstrap_icon_options
+  include CategoriesHelper
   def index
     @categories = current_user.categories.includes(:payments)
   end
@@ -15,28 +15,6 @@ class CategoriesController < ApplicationController
     else
       render 'new'
     end
-  end
-
-  def bootstrap_icon_options
-    [
-      ['Select an Icon', ''],
-      ['Book', 'bi bi-book'],
-      ['Food', 'bi bi-cup-straw'],
-      ['Drink', 'bi bi-beer'],
-      ['Rent', 'bi bi-house-fill'],
-      ['Car', 'bi bi-car'],
-      ['Gas', 'bi bi-fuel-pump'],
-      ['Electricity', 'bi bi-lightbulb-fill'],
-      ['Water', 'bi bi-droplet-fill'],
-      ['Phone', 'bi bi-phone'],
-      ['Internet', 'bi bi-wifi'],
-      ['Clothes', 'bi bi-tshirt'],
-      ['Online Shopping', 'bi bi-cart'],
-      ['Health', 'bi bi-heart-fill'],
-      ['Gym', 'bi bi-person-arms-up'],
-      ['Travel', 'bi bi-geo-alt-fill'],
-      ['Education', 'bi bi-book-half']
-    ]
   end
 
   private
